da478316ba835d28c9b50beb39da4b7a65ab353d
yangjiseonn <yangjiseonn@gmail.com>
2025-10-19 13:12:42 +0900

    [FIX] 장바구니 수량 누적 수정

--- src/main/java/com/sampoom/backend/api/cart/entity/AgencyCartItem.java
+++ src/main/java/com/sampoom/backend/api/cart/entity/AgencyCartItem.java
@@ -31,6 +31,6 @@ public class AgencyCartItem {
     private int quantity;
 
     public void updateQuantity(int quantity) {
-        this.quantity = quantity;
+        this.quantity += quantity;
     }
 }
